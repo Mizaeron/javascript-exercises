5ef132aa5555139dd5ce6aed7889b81cf3579c99
Miza <mizaeron@gmail.com>
2024-07-21 19:09:44 +0100

    create a function with a loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, times) {   
+    let result = "";
+  for (let i = 0; i < times; i++) {
+   result = result + string;
+  }
 
-};
+  return result;
+    };
+
+
+
+
+
+
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
