afd5dfc192f91c9b15d61fc203e9197877201415
Miza <mizaeron@gmail.com>
2024-07-26 14:45:13 +0100

    Solutions to 3, 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removeValues) {
+ 
+    return array.filter(item => !removeValues.includes(item));
+}
 
+
+
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0;
+if (a < 0 || b < 0 || 
+    a % 1 !== 0 || typeof b !== 'number') {
+    return "ERROR";
+}
 
-};
+else if (a >= b) {
+
+while (a >= b) {
+    result = result + a;
+    a--;
+}
+} else {  
+
+while (a <= b) {
+   result = result + a;
+    a++;
+}
+}
+    return result;
+}
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
