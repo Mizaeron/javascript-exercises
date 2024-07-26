a2958d2b3c3e03a98b697bac6690a680d33586e9
Miza <mizaeron@gmail.com>
2024-07-26 15:40:26 +0100

    Solve conditionals for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    
+    if (years % 400 == 0) {
+        return true;
+    } else if  (years % 4 == 0 && years % 100 == 0) {
+        return false;
+    } else if (years % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+
+
+console.log(leapYears(2000));
 // Do not edit below this line
 module.exports = leapYears;
