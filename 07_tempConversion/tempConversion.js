8b2d594fc00301b888bebf25b053125ad782c94f
Miza <mizaeron@gmail.com>
2024-07-28 19:17:42 +0100

    Solve temperature differences

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let F;
+  F = (farenheit - 32) * 5/9;
+  let rounded = Math.round(F * 10) / 10;
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let C;
+  C = (celcius * 9/5 + 32);
+  let rounded = Math.round(C * 10) / 10;
+
+  return rounded;
 };
 
+
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
